ef889593f9ce676cd6fe766bc20fe8f7a83ffd16
Denis Poisson <poisson_1@hotmail.com>
2015-09-11 15:35:12 +0200

    Update app.js

--- JavaScript/recursive-innerHTML-looper/app.js
+++ JavaScript/recursive-innerHTML-looper/app.js
@@ -1,16 +1,16 @@
 var changeableText = document.getElementById("text");
-var listOfTests = ["Text 1", "Text 2", "Text 3", "awesome", "this rules"];
+var listOfTexts = ["Text 1", "Text 2", "Text 3", "awesome", "this rules"];
 var i = 1;
 
 var setText = function (indx) {
-    changeableText.innerHTML = listOfTests[indx];
+    changeableText.innerHTML = listOfTexts[indx];
 };
 
 var runit = function () {
     
     setTimeout(function () {
         setText(i);
-        if (i < (listOfTests.length - 1)) {
+        if (i < (listOfTexts.length - 1)) {
             i = i + 1;
         } else {
             i = 0;
